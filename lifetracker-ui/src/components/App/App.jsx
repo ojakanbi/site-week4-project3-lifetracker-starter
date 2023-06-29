d1703af7b8bfa2f345514784f6cc64876778af30
oyindamola <oyindamola.akanbi@oyindam-ltmujtm.internal.salesforce.com>
2023-06-29 02:08:33 -0700

    user register data passes through the data

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -2,15 +2,48 @@ import Navbar from '../Navbar/Navbar'
 import Home from '../Home/Home'
 import './App.css'
 import React from 'react'
+import axios from 'axios'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Register from '../Register/Register'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+
 
 
 function App() {
+  const [user, setUser] = useState(
+    {firstname: '', lastname: '', emailaddress: '', username: '',password: ''}
+    )
+  console.log(user) // handling the api request to register the user
 
+//   //create a async function for a register user endpoint 
+// useEffect(() => {
+//   const registerUser = async () => {
+//     try {
+//       const response = await axios.post('http://localhost:3001/auth/register',user)
+
+//       if (response.status !== 200) {
+//         throw new Error('Unable to register user')
+//       }
+//       return response.data
+//       console.log(response)
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+//   registerUser()
+// }, [])
+
+
+  
   return (
     <div className='app-container'>
 
       <Navbar />
       <Home />
+      <Register user={user} setUser={setUser}/>
+
 
     </div>
   )
---
+++ lifetracker-ui/src/components/Register/Register.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import axios from "axios";
+
+
+export default function Register({user, setUser}) {
+    function handleChange(event) {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+         // Add logic to make the API request to register the user using axios
+   
+    axios.post('http://localhost:3001/auth/register', user)
+      .then(response => {
+        console.log('User registered successfully:', response.data);
+        // Handle successful registration
+      })
+      .catch(error => {
+        console.error('Error registering user:', error);
+        // Handle error during registration
+      });
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className='grid'></div>
+            <label htmlFor='firstname'>First Name
+                <input type='text' name='firstname' value={user.firstname} onChange={handleChange} required />
+            
+            </label>
+            <label htmlFor='lastname'>Last Name 
+                <input type='text' name='lastname' value={user.lastname} onChange={handleChange} required />
+            </label>
+            <label htmlFor='emailaddress'>Email Address 
+                <input type='email' name='emailaddress' value={user.emailaddress} onChange={handleChange} required />
+            </label>
+            <label htmlFor='username'>Username
+                <input type='text' name='username' value={user.username} onChange={handleChange} required />
+            </label>
+            <label htmlFor='password'>Password 
+                <input type='password' name='password' value={user.password} onChange={handleChange} required />
+            </label>
+            <button type='submit'>Register</button>
+
+            
+
+
+        </form>
+    )
+}
