b06d607fae777f142f3758cd447e9bea5968d0d2
oyindamola <oyindamola.akanbi@oyindam-ltmujtm.internal.salesforce.com>
2023-07-06 18:31:12 -0700

    added hours calculation into the sleep page

--- lifetracker-ui/src/components/SleepPage/SleepData.jsx
+++ lifetracker-ui/src/components/SleepPage/SleepData.jsx
@@ -28,14 +28,24 @@ export default function SleepData({ sleepState, setSleepState }) {
     return formattedDateTime;
   }
   
-
+  function calculateHoursSlept(startDateTime, endDateTime) {
+    const start = new Date(startDateTime);
+    const end = new Date(endDateTime);
+    const diffInMilliseconds = end - start;
+    const diffInHours = diffInMilliseconds / (1000 * 60 * 60); // Convert milliseconds to hours
+    return diffInHours;
+  }
+  
   return (
     <div className="sleep-wrapper">
       <h1>Sleep Log</h1>
       <div className="sleepData-container">
-        
-        {sleepData.sort((a,b) => b.id - a.id).map((sleep, index) => (
+        {sleepData.sort((a, b) => b.id - a.id).map((sleep, index) => (
           <div className="sleepData-item" key={index}>
+            <div className="sleep-hours">
+            <p>{calculateHoursSlept(sleep.start_time, sleep.end_time)}</p>
+            </div>
+  
             <h2>Start Time</h2>
             <p>{formatDate(sleep.start_time)}</p>
             <h2>End Time</h2>
@@ -45,4 +55,6 @@ export default function SleepData({ sleepState, setSleepState }) {
       </div>
     </div>
   );
+  
+
 }
