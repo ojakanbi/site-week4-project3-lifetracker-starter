e7657c846f164862fb60a0f2941b30bddb421a8f
oyindamola <oyindamola.akanbi@oyindam-ltmujtm.internal.salesforce.com>
2023-07-07 11:40:13 -0700

    css for the activity page checked

--- lifetracker-ui/src/components/ActivityPage/ActivityPage.jsx
+++ lifetracker-ui/src/components/ActivityPage/ActivityPage.jsx
@@ -1,22 +1,59 @@
 import React from "react";
+import "./ActivityPage.css";
 
-export default function ActivityPage({navbar}) {
-    return (
-        <>
-        {navbar ? (
-            <div>
-                <h1>
-                    hey
-                </h1>
+export default function ActivityPage({ navbar }) {
+  return (
+    <>
+      {navbar ? (
+        <div className="activityPage-container">
+          <div className="activityContent-container">
+            <div className="activityHeader">
+              <h1>Activity</h1>
+              <div className="activityHeader-right">
+                <button className="activityButton-e">Add Exercise</button>
+                <button className="activityButton-n">Log Sleep</button>
+                <button className="activityButton-s">Record Nutrition</button>
+              </div>
             </div>
-        ): (
-            <div>
-                <h1>
-                   Sign in to view Activity data
-                </h1>
-            </div>
-        )}
-        </>
+            <div className="activityFeed-container">
+              <div className="activityFeed-item">
+                <div className="activityFeed-item-left">
+                  <h2 className="cardHeader">Total Exercise Minutes</h2>
+                </div>
+                <div className="mainStat">
+                  <h2>0</h2>
+                  <p>-2.2</p>
+                  
+                </div>
+              </div>
+              <div className="activityFeed-item">
+                <div className="activityFeed-item-left">
+                  <h2 className="cardHeader">Average Hours of Sleep</h2>
+                </div>
+                <div className="mainStat">
+                  <h2>0</h2>
+                  <p>+2.1</p>
 
-    )
+                </div>
+              </div>
+              <div className="activityFeed-item">
+                <div className="activityFeed-item-left">
+                  <h2 className="cardHeader">Average Daily Calories</h2>
+                </div>
+                <div className="mainStat">
+                  <h2>0</h2>
+                  <p>-2.5</p>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>Sign in to view Activity data</h1>
+        </div>
+      )}
+    </>
+  );
 }
