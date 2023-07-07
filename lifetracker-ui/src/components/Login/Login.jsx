27f5b66c94d699ce9f136ad52849feb7ff04e4ae
oyindamola <oyindamola.akanbi@oyindam-ltmujtm.internal.salesforce.com>
2023-07-06 17:21:03 -0700

    login error completed

--- lifetracker-ui/src/components/Login/Login.jsx
+++ lifetracker-ui/src/components/Login/Login.jsx
@@ -1,55 +1,66 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./login.css";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import login from "../../images/login.png"
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import login from "../../images/login.png";
 
-export default function Login({ loginUser, setLoginUser, navbar, setNavbar, userInfo, setUserInfo}) {
-
+export default function Login({
+  loginUser,
+  setLoginUser,
+  navbar,
+  setNavbar,
+  userInfo,
+  setUserInfo,
+}) {
+  const [error, setError] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    axios.post('http://localhost:3001/auth/login', loginUser)
-      .then(response => {
-        console.log('User logged in successfully:', response.data);
+    axios
+      .post("http://localhost:3001/auth/login", loginUser)
+      .then((response) => {
+        console.log("User logged in successfully:", response.data);
         setUserInfo(response.data.user);
-        localStorage.setItem('userId', response.data.user.id);
+        localStorage.setItem("userId", response.data.user.id);
         console.log("user info: ", userInfo);
-        localStorage.setItem('token', response.data.token);
-         window.location = "/sleep"; // Redirect to the secret page upon successful login
+        localStorage.setItem("token", response.data.token);
+        window.location = "/sleep"; // Redirect to the secret page upon successful login
         // Handle successful login
-        
-        
-      
       })
-      .catch(error => {
-        console.error('Error logging in user:', error);
+      .catch((error) => {
+        console.error("Error logging in user:", error.response.data);
+        console.error("Error adding sleep data:", error);
+        const errorResponse = error.response.data;
+        const errorMessage = errorResponse.match(/Error: (.*?)<br>/)[1];
+        setError(errorMessage);
         // Handle error during login
       });
-      
   }
 
   function handleChange(event) {
     setLoginUser({
       ...loginUser,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   }
-
+  console.log(error);
   return (
     <>
-    
-
       <div className="login-container">
-      <div className="form-title">
-            <img className = "login-img" src={login}></img>
-            <h1>Welcome</h1>
+        <div className="form-title">
+          <img className="login-img" src={login}></img>
+          <h1>Welcome</h1>
+        </div>
+
+        {error && (
+          <div className="error-container">
+            <h3>{error}</h3>
           </div>
-       
+        )}
+
         <div className="form-container">
-         
           <div>
             <form className="form" onSubmit={handleSubmit}>
               <label htmlFor="emailaddress">
@@ -76,16 +87,10 @@ export default function Login({ loginUser, setLoginUser, navbar, setNavbar, user
             </form>
           </div>
         </div>
-       
       </div>
       <div className="user-login">
-      {userInfo.user && 
-          <h3>{userInfo.user.username} Login successful!!</h3>
-          
-          }
-
+        {userInfo.user && <h3>{userInfo.user.username} Login successful!!</h3>}
       </div>
-     
     </>
   );
 }
--- lifetracker-ui/src/components/Navbar/Navbar.jsx
+++ lifetracker-ui/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../images/logo.png"
+import {useNavigate} from "react-router-dom";
+
 
 
 import { Link, Outlet } from "react-router-dom";
 
 export default function Navbar({navbar, setNavbar }) {
+
+ const navigate = useNavigate();
   return (
     <div className="nav-container">
       <nav className="Navbar">
         <div>
-          <img src={logo} className="logo"></img>
+          <a href= "/" className="logo-container">
+          <img src={logo}  className="logo"></img>
+          </a>
         </div>
         <ul>
           <li>
